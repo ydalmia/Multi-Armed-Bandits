a469d6ead749c904de4cb064453b3f3a02a7fe19
Yash Dalmia <yashdalmia111@gmail.com>
2023-03-02 07:53:31 -0800

    add more MAB tests

--- mab.jl
+++ mab.jl
@@ -75,17 +75,24 @@ function RandomMAB(
 	return MAB(n, m, T, r, γ)
 end
 
-function basic_compile_tests()
+function test_MAB_compiles()
+	random_mab = RandomMAB(3, (2, 4, 7), 0.7, rng=MersenneTwister(1))
+	return random_mab
+end
+
+function test_MAB_consistent_distributions()
 	mdp = RandomMAB(3, (2, 4, 7), 0.7, rng=MersenneTwister(1))
 	@assert has_consistent_distributions(mdp)
 end
 
-function simulate_random_policy()
+function test_MAB_valid_transitions()
 	n = 3 # number of arms
 	m = (2, 4, 7) # state space for each arm
 	γ = 0.7
 	mdp = RandomMAB(n, m, γ, rng=MersenneTwister(1))
 	
+	println("Results of MAB Simulation: ")
+	println("---------------------------")
 	# run a simulation
 	history = simulate(
 		HistoryRecorder(max_steps=10), 
@@ -102,13 +109,13 @@ function simulate_random_policy()
 
 	all_transitions_valid = true
 	for (s, a, r, sp) in eachstep(history, "(s, a, r, sp)")
-		valid_transition = validMABTransition(s, a, sp)
-		all_transitions_valid = valid_transition && all_transitions_valid 
+		all_transitions_valid = validMABTransition(s, a, sp) && all_transitions_valid 
 		println("reward $(r) received when state $(sp) was reached after action $(a) was taken in state $(s)")
 	end
 
 	@assert all_transitions_valid
 end
 
-basic_compile_tests()
-simulate_random_policy()
+test_MAB_compiles()
+test_MAB_consistent_distributions()
+test_MAB_valid_transitions()
